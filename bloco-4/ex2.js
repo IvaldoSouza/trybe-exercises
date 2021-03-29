390cb4ee6b2c7866c15bbde02279b033e9bad59d
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-03-29 06:04:48 -0300

    exercicios bloco4 ex3

--- bloco-4/ex2.js
+++ bloco-4/ex2.js
@@ -1,7 +1,7 @@
-let n = 8;
+let n = 5;
 
 if (n > 1){
-    for(let i = 0; i < n; i += 1) {
-        console.log('*'.repeat(n));
+    for(let i = 0; i <= n; i += 1) {
+        console.log('*'.repeat(i));
     }       
 }
