62fedf14314dae8c43669a8bdbb9aadf7f763462
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-03-29 15:12:58 -0300

    exercicios bloco4 ex4

---
+++ bloco-4/ex4.js
@@ -0,0 +1,13 @@
+
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        goden: 2,
+        silver: 3,
+    }
+}
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' de idade.')
