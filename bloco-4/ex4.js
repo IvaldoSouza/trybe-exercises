0b989e5d6a066320329291ad9be662386a3c1c55
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-03-29 15:28:31 -0300

    exercicios bloco4 ex4-2

--- bloco-4/ex4.js
+++ bloco-4/ex4.js
@@ -1,5 +1,3 @@
-
-
 let player = {
     name: 'Marta',
     lastName: 'Silva',
@@ -9,5 +7,8 @@ let player = {
         silver: 3,
     }
 }
+let bestInTheWord = [2006, 2007, 2008, 2009, 2010, 2018];
+
 
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' de idade.')
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo ' + bestInTheWord.length + ' vezes')
